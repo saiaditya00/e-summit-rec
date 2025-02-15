7ad2564497ec34fe2203b61d458022dc732ee511
saiaditya00 <kamarsuaditya@gmail.com>
2025-02-15 16:25:09 +0530

    event page

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import Contact from './pages/Contact';
 function App() {
   return (
     <>
+    
     <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/profile" element={<Profile />} />
